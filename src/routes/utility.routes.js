c13cf73b86aa558e6946bb1348380c2c794ae620
Ajay Kumar <ajayasrdeveloper1@gmail.com>
2024-09-10 02:27:20 -0700

    helping api

--- src/routes/utility.routes.js
+++ src/routes/utility.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { userVerify } from "../middlewares/userAuth.js";
-import { getPackageList, getUserList } from "../controllers/utility.controller.js";
+import { getPackageList, getPayInApiList, getPayOutApiList, getUserList } from "../controllers/utility.controller.js";
 const router = express.Router();
 
 router.get("/getUserList", userVerify, getUserList);
 
 router.get("/getPackageList", userVerify, getPackageList);
+
+router.get("/getPayInApiList", userVerify, getPayInApiList);
+
+router.get("/getPayOutApiList", userVerify, getPayOutApiList);
 
 export default router;
