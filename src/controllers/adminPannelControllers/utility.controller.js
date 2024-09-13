8e31498b04898112d47fc6e700f38a372964a0d4
Ajay Kumar <ajayasrdeveloper1@gmail.com>
2024-09-13 14:19:19 +0530

    push the api helping

--- src/controllers/adminPannelControllers/utility.controller.js
+++ src/controllers/adminPannelControllers/utility.controller.js
@@ -15,6 +15,16 @@ export const getUserList = asyncHandler(async (req, res) => {
     res.status(200).json(new ApiResponse(200, userInfo))
 })
 
+export const getUserListWithUpiWallet = asyncHandler(async (req, res) => {
+    let userInfo = await userDB.aggregate([{ $match: { memberType: "Users" } }, {
+        $project: { "_id": 1, "memberId": 1, "fullName": 1, "upiWalletBalance": 1 }
+    }, { $sort: { createdAt: -1 } }])
+    if (!userInfo.length) {
+        return res.status(400).json({ message: "Failed", data: "Not Active User Avabile !" })
+    }
+    res.status(200).json(new ApiResponse(200, userInfo))
+})
+
 export const getPackageList = asyncHandler(async (req, res) => {
     let packageData = await packageModel.aggregate([{ $match: { isActive: true } }, {
         $project: { "_id": 1, "packageName": 1 }
--- src/routes/adminPannelRoutes/utility.routes.js
+++ src/routes/adminPannelRoutes/utility.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { userVerify } from "../../middlewares/userAuth.js";
-import { getPackageList, getPayInApiList, getPayOutApiList, getUserList } from "../../controllers/adminPannelControllers/utility.controller.js";
+import { getPackageList, getPayInApiList, getPayOutApiList, getUserList, getUserListWithUpiWallet } from "../../controllers/adminPannelControllers/utility.controller.js";
 const router = express.Router();
 
 router.get("/getUserList", userVerify, getUserList);
+
+router.get("/getUserWithUpiWallet", userVerify, getUserListWithUpiWallet);
 
 router.get("/getPackageList", userVerify, getPackageList);
 
