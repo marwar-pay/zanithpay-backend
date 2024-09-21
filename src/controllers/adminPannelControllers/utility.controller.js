ed7af45fe1e0691302ec06179271d354c2170dd9
Ajay Kumar <ajayasrdeveloper1@gmail.com>
2024-09-21 14:42:00 +0530

    add the utility route and controllers

--- src/controllers/adminPannelControllers/utility.controller.js
+++ src/controllers/adminPannelControllers/utility.controller.js
@@ -1,6 +1,7 @@
 import userDB from "../../models/user.model.js";
 import packageModel from "../../models/package.model.js";
 import apiPayInModel from "../../models/apiPayInSwitch.model.js";
+import ticketInModel from "../../models/supportTicket.model.js";
 import apiPayOutModel from "../../models/apiPayOutSwitch.model.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
@@ -17,7 +18,17 @@ export const getUserList = asyncHandler(async (req, res) => {
 
 export const getUserListWithWallet = asyncHandler(async (req, res) => {
     let userInfo = await userDB.aggregate([{ $match: { memberType: "Users" } }, {
-        $project: { "_id": 1, "memberId": 1, "fullName": 1, "upiWalletBalance": 1,"EwalletBalance": 1 }
+        $project: { "_id": 1, "memberId": 1, "fullName": 1, "upiWalletBalance": 1, "EwalletBalance": 1 }
+    }, { $sort: { createdAt: -1 } }])
+    if (!userInfo.length) {
+        return res.status(400).json({ message: "Failed", data: "Not Active User Avabile !" })
+    }
+    res.status(200).json(new ApiResponse(200, userInfo))
+})
+
+export const getAllMemberList = asyncHandler(async (req, res) => {
+    let userInfo = await userDB.aggregate([{
+        $project: { "_id": 1, "memberId": 1, "fullName": 1 }
     }, { $sort: { createdAt: -1 } }])
     if (!userInfo.length) {
         return res.status(400).json({ message: "Failed", data: "Not Active User Avabile !" })
@@ -50,6 +61,28 @@ export const getPayInApiList = asyncHandler(async (req, res) => {
 
     if (!apiPayIn.length) {
         return res.status(400).json({ message: "Failed", data: "Not Payin Api Found !" })
+    }
+    res.status(200).json(new ApiResponse(200, apiPayIn))
+})
+
+export const getAllGenTicketList = asyncHandler(async (req, res) => {
+    let apiPayIn = await ticketInModel.aggregate([{
+        $project: { "_id": 1, "TicketID": 1, "relatedTo": 1 }
+    }, { $sort: { createdAt: -1 } }])
+
+    if (!apiPayIn.length) {
+        return res.status(400).json({ message: "Failed", data: "Not Ticket Found !" })
+    }
+    res.status(200).json(new ApiResponse(200, apiPayIn))
+})
+
+export const getPendingTicketList = asyncHandler(async (req, res) => {
+    let apiPayIn = await ticketInModel.aggregate([{ $match: { $expr: { isStatus: "Pending" } } }, {
+        $project: { "_id": 1, "TicketID": 1, "relatedTo": 1 }
+    }, { $sort: { createdAt: -1 } }])
+
+    if (!apiPayIn.length) {
+        return res.status(400).json({ message: "Failed", data: "Not Pending Ticket Found !" })
     }
     res.status(200).json(new ApiResponse(200, apiPayIn))
 })
--- src/routes/adminPannelRoutes/utility.routes.js
+++ src/routes/adminPannelRoutes/utility.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { userVerify } from "../../middlewares/userAuth.js";
-import { getPackageList, getPayInApiList, getPayOutApiList, getUserList, getUserListWithWallet } from "../../controllers/adminPannelControllers/utility.controller.js";
+import { getAllGenTicketList, getAllMemberList, getPackageList, getPayInApiList, getPayOutApiList, getPendingTicketList, getUserList, getUserListWithWallet } from "../../controllers/adminPannelControllers/utility.controller.js";
 const router = express.Router();
 
 router.get("/getUserList", userVerify, getUserList);
+
+router.get("/getAllMemberList", userVerify, getAllMemberList);
 
 router.get("/getUserWithWallet", userVerify, getUserListWithWallet);
 
@@ -12,5 +14,9 @@ router.get("/getPackageList", userVerify, getPackageList);
 router.get("/getPayInApiList", userVerify, getPayInApiList);
 
 router.get("/getPayOutApiList", userVerify, getPayOutApiList);
+
+router.get("/getAllGenTicketList", userVerify, getAllGenTicketList);
+
+router.get("/getPendingTicketList", userVerify, getPendingTicketList);
 
 export default router;
