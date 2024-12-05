ccb4989d013feb2211cd31d0956e807dd88ac235
ajay <ajayasrdeveloper1@gmail.com>
2024-12-05 16:06:35 +0530

    added

--- src/middlewares/apiValidate.js
+++ src/middlewares/apiValidate.js
@@ -1,42 +1,37 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import requestIp from "request-ip";
 import userDB from "../models/user.model.js";
 import ipWhiteListDB from "../models/ipWhiteList.model.js"
 
 export const apiValidate = asyncHandler(async (req, res, next) => {
     try {
-        // const clientId = req.headers['x-client-id'];  // Get client ID from request header (or other method)
-        // const clientIp = req.ip || req.connection.remoteAddress;
-        // const formattedIp = clientIp.includes('::ffff:') ? clientIp.split(':').pop() : clientIp;
+        const clientIp = requestIp.getClientIp(req);
+        let user = await userDB.aggregate([{ $match: { $and: [{ userName: req?.body?.userName }, { trxAuthToken: req?.body?.authToken }, { isActive: true }] } }])
 
-        // let user = await userDB.aggregate([{ $match: { $and: [{ userName: req?.body?.userName }, { trxAuthToken: req?.body?.authToken }, { isActive: true }] } }])
+        if (true) {
+            console.log(clientIp, "clientIp");
+            return res.status(200).json({ data: clientIp })
+        }
 
-        // if (true) {
-        //     console.log(clientId, "clientId");
-        //     console.log(formattedIp, "formattedIp");
-        //     console.log(clientIp, "clientIp");
-        //     return res.status(200).json({ clientId, clientIp, formattedIp })
-        // }
+        if (user.length === 0) {
+            return res.status(400).json({ message: "Failed", data: "Invalid User or InActive user Please Try again !" })
+        }
 
-        // if (user.length === 0) {
-        //     return res.status(400).json({ message: "Failed", data: "Invalid User or InActive user Please Try again !" })
-        // }
+        let getUserIpList = await ipWhiteListDB.findOne({ memberId: user[0]._id });
 
-        // let getUserIpList = await ipWhiteListDB.findOne({ memberId: user[0]._id });
+        if (!getUserIpList) {
+            return res.status(400).json({ message: "Failed", data: `Please required IP Whitelist Your Current IP : ${formattedIp}` })
+        }
 
-        // if (!getUserIpList) {
-        //     return res.status(400).json({ message: "Failed", data: `Please required IP Whitelist Your Current IP : ${formattedIp}` })
-        // }
+        if (getUserIpList?.ipUserDev === "*") {
+            next()
+        }
+        else if (getUserIpList?.ipUser === formattedIp || getUserIpList?.ipUserDev === formattedIp) {
+            next()
+        }
 
-        // if (getUserIpList?.ipUserDev === "*") {
-        //     next()
-        // }
-        // else if (getUserIpList?.ipUser === formattedIp || getUserIpList?.ipUserDev === formattedIp) {
-        //     next()
-        // }
-        next()
-
-        // return res.status(400).json({ message: "Failed", data: `Please required IP Whitelist Your Current IP : ${formattedIp}` })
+        return res.status(400).json({ message: "Failed", data: `Please required IP Whitelist Your Current IP : ${formattedIp}` })
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid Request !");
     }
