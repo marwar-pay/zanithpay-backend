5a9a3f89f968fc93fcd1a11b3a68127ba3c186a3
ajay <ajayasrdeveloper1@gmail.com>
2024-12-05 16:25:28 +0530

    commmit ip io

--- src/middlewares/apiValidate.js
+++ src/middlewares/apiValidate.js
@@ -8,18 +8,15 @@ export const apiValidate = asyncHandler(async (req, res, next) => {
     try {
         const clientIp = requestIp.getClientIp(req);
         let user = await userDB.aggregate([{ $match: { $and: [{ userName: req?.body?.userName }, { trxAuthToken: req?.body?.authToken }, { isActive: true }] } }])
-        console.log(typeof clientIp)
         if (user.length === 0) {
             return res.status(400).json({ message: "Failed", data: "Invalid User or InActive user Please Try again !" })
         }
 
-
         let getUserIpList = await ipWhiteListDB.findOne({ memberId: user[0]._id });
 
         if (!getUserIpList) {
-            return res.status(400).json({ message: "Failed", data: `Please required IP Whitelist Your Current IP : ${clientIp}`, main: inside })
+            return res.status(400).json({ message: "Failed", data: `Please required IP Whitelist Your Current IP : ${clientIp}` })
         }
-
         if (getUserIpList?.ipUserDev == "*") {
             next()
         }
