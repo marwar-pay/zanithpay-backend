61d7993322377a10f7ba9aa22ea3864547db353e
ajay <ajayasrdeveloper1@gmail.com>
2024-12-16 15:06:33 +0530

    old and new collection concept

---
+++ src/models/oldQrGeneration.model.js
@@ -0,0 +1,43 @@
+import { Schema, model } from "mongoose";
+
+const oldQrGenerationSchema = new Schema({
+    memberId: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+        required: [true, "Please Select Member Id !"]
+    },
+    trxId: {
+        type: String,
+        trim: true,
+        index: true,
+        unique: [true, "Trx Id should be Unique"],
+        required: [true, "Required TrxId !"]
+    },
+    refId: {
+        type: String,
+    },
+    amount: {
+        type: Number,
+        required: [true, "Required Amount !"]
+    },
+    name: {
+        type: String,
+        required: [true, "Required Name !"]
+    },
+    ip: {
+        type: String,
+    },
+    qrData: {
+        type: String,
+    },
+    qrIntent: {
+        type: String,
+    },
+    callBackStatus: {
+        type: String,
+        enum: ["Pending", "Failed", "Success"],
+        default: "Pending"
+    },
+}, { timestamps: true });
+
+export default new model("oldQrGenerationRecode", oldQrGenerationSchema.index({ createdAt: 1 }));
