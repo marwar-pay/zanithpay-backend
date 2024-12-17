0480c548985e6ab4f25a0080aee81f88f8532679
ajay <ajayasrdeveloper1@gmail.com>
2024-12-17 16:06:38 +0530

    added

--- src/utils/scheduleTask.js
+++ src/utils/scheduleTask.js
@@ -14,7 +14,7 @@ function scheduleWayuPayOutCheck() {
         try {
             let GetData = await payOutModelGenerate.find({ isSuccess: "Pending" }).limit(500);
             if (GetData.length !== 0) {
-                GetData.forEach(async(item) => {
+                GetData.forEach(async (item) => {
                     let uatUrl = "https://api.waayupay.com/api/api/api-module/payout/status-check"
                     let postAdd = {
                         clientId: "adb25735-69c7-4411-a120-5f2e818bdae5",
@@ -29,14 +29,11 @@ function scheduleWayuPayOutCheck() {
                     }
 
                     await axios.post(uatUrl, postAdd, header).then(async (data) => {
-                        console.log(data?.data)
                         if (data?.data?.status !== 1) {
-                            console.log("Failed")
                             await payOutModelGenerate.findByIdAndUpdate(item._id, { isSuccess: "Failed" })
                         }
 
                         else if (data?.data?.status === 1) {
-                            console.log("success")
                             let userWalletInfo = await userDB.findById(item?.memberId, "_id EwalletBalance");
                             let beforeAmountUser = userWalletInfo.EwalletBalance;
                             let finalEwalletDeducted = item?.afterChargeAmount;
